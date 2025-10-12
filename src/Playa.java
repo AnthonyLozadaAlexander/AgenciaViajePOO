c9796fca32ac6c1faf633e9be3587e054cf45a6d
zDefcon <antholozadarequelme@gmail.com>
2025-10-11 23:13:31 -0500

    refactor(Playa): Corrige la logica de calculo y typo en la clase Playa

--- src/Playa.java
+++ src/Playa.java
@@ -7,61 +7,58 @@
  *
  * @author PC 15
  */
-public class Playa extends Paquete{
-    
+public class Playa extends Paquete {
+
     private boolean todoIncluido;
-    private double  impuestoTuristico;
-    private double  descuentoTemporada;
-    
-    public Playa(){
-	super();
+    private double impuestoTuristico;
+    private double descuentoTemporada;
+
+    public Playa() {
+        super();
     }
-    
-    public Playa(boolean todoIncluido, double impuestoTuristico, double descuentoTemporada){
+
+    public Playa(boolean todoIncluido, double impuestoTuristico, double descuentoTemporada) {
         this.todoIncluido = todoIncluido;
         this.impuestoTuristico = impuestoTuristico;
         this.descuentoTemporada = descuentoTemporada;
     }
-    
-    public Playa(String destino, int dias, double precioBase, double impuestoTuristico, double descuentoTemporada){
-	super(destino, dias, precioBase);
-	this.impuestoTuristico = impuestoTuristico;
-	this.descuentoTemporada = descuentoTemporada;
+
+    public Playa(String destino, int dias, double precioBase, double impuestoTuristico, double descuentoTemporada) {
+        super(destino, dias, precioBase);
+        this.impuestoTuristico = impuestoTuristico;
+        this.descuentoTemporada = descuentoTemporada;
     }
-    
-    public void setImpuestoTuristico(double impuestoTuristico){
-	this.impuestoTuristico = impuestoTuristico;
+
+    public void setImpuestoTuristico(double impuestoTuristico) {
+        this.impuestoTuristico = impuestoTuristico;
     }
-    
-    public void setDescuentoTemporada(double descuentoTemporada){
-	this.descuentoTemporada = descuentoTemporada;
+
+    public void setDescuentoTemporada(double descuentoTemporada) {
+        this.descuentoTemporada = descuentoTemporada;
     }
-    
-    public void setTodoIncluido(boolean todoIncluido){
-	this.todoIncluido = todoIncluido;
+
+
+    public double calcularDescuento() {
+        return getPrecioBase() * descuentoTemporada / 100;
     }
-    
-    public double calcularDescuento(){
-	return getPrecioBase() * descuentoTemporada/100;
+
+    public double calcularImpuesto() {
+        return getPrecioBase() * impuestoTuristico / 100;
     }
-    
-    public double calcularImpuesto(){
-	return getPrecioBase() * impuestoTuristico/100; 
+
+    public double calcularTotal(boolean todoIncluido) {
+        double subtotal = getPrecioBase() - calcularDescuento() + calcularImpuesto();
+        if (todoIncluido) {
+            return subtotal + 200;
+        } else {
+            return subtotal;
+        }
     }
-    
-    public double calcularTotal(boolean todoInlcuido){
-	if(todoIncluido == true){
-	    
-return ((getPrecioBase() - descuentoTemporada) + impuestoTuristico) + 200; 
-	}	
-	return (getPrecioBase() - descuentoTemporada + impuestoTuristico);
+
+    public String resumenInfo() {
+
+        return super.mostrarInfo() + "\nDescuento: " + calcularDescuento() + "\nImpuesto: " + calcularImpuesto() + "\nTotal: " + calcularTotal(todoIncluido) + "\n";
+
     }
-    
-    public String resumenInfo(){
-	
-return super.mostrarInfo() + "\nDescuento: " + calcularDescuento() + "\nImpuesto: " + calcularImpuesto() + "\nTotal: " + calcularTotal(todoIncluido) + "\n";
-	
-    }
-    
-    
+
 }
